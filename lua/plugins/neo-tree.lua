22b8de3bdd9c1bdf766d54fe7824bed68fafc41b
Ole Henning Vårdal <olevaar@gmail.com>
2025-09-24 11:40:31 +0200

    Update config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,15 +9,15 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  lazy = false,
+  lazy = true,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<C-b>', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['<C-b>'] = 'close_window',
         },
       },
     },
