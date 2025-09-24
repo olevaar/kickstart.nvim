3ce46afd448dc5727bd960cb167d7e4f90813cc2
Ole Henning Vårdal <ole-henning.vardal@soprasteria.com>
2025-09-24 16:21:47 +0200

    Add insert-mode navigation

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -3,6 +3,8 @@ local M = {}
 M.keymaps = {
   { 'i', '<C-h>', '<Left>', { desc = 'Move left' } },
   { 'i', '<C-l>', '<Right>', { desc = 'Move right' } },
+  { 'i', '<C-j>', '<Down>', { desc = 'Move down' } },
+  { 'i', '<C-k>', '<Up>', { desc = 'Move up' } },
   { 'i', '<C-b>', '<Home>', { desc = 'Beginning of line' } },
   { 'i', '<C-e>', '<End>', { desc = 'End of line' } },
 }
