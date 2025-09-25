137d7a7e6c53d72441232f9a0a96709b78d449aa
Ole Henning Vårdal <olevaar@gmail.com>
2025-09-25 12:24:46 +0200

    Update keymaps and noice search

--- init.lua
+++ init.lua
@@ -1,55 +1,35 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
 vim.g.have_nerd_font = true
-
 vim.o.number = true
-
 vim.o.mouse = 'a'
-
 vim.o.showmode = false
-
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
-
 vim.o.breakindent = true
-
 vim.o.undofile = true
-
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
 vim.o.signcolumn = 'yes'
-
 vim.o.updatetime = 250
-
 vim.o.timeoutlen = 300
-
 vim.o.splitright = true
 vim.o.splitbelow = true
-
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
 vim.o.inccommand = 'split'
-
 vim.o.cursorline = true
-
 vim.o.scrolloff = 10
-
 vim.o.confirm = true
-
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Load custom mappings
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
 for _, keymap in ipairs(require('custom.mappings').keymaps) do
   vim.keymap.set(unpack(keymap))
 end
