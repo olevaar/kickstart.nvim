bb5a08c0313e87293aad8f109c285e233e400c0e
Ole Henning Vårdal <olevaar@gmail.com>
2025-09-25 15:01:14 +0200

    Add language servers to config.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -114,6 +114,10 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
+      'cssls',
+      'html',
+      'ts_ls',
+      'angularls',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
