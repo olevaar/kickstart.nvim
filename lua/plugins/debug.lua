7694480492e7ef5aa80fd3248b639c63bb67690d
Ole Henning Vårdal <olevaar@gmail.com>
2025-10-15 10:17:34 +0200

    Keep dapui open when application unexpectedly terminates

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -102,8 +102,6 @@ return {
     }
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     require('dap-go').setup {
       delve = {
