90b7b1f9b4990ca7256041ae9fe4eab954dc7595
Ole Henning Vårdal <olevaar@gmail.com>
2025-10-16 23:41:19 +0200

    Fix broken prints

--- install.sh
+++ install.sh
@@ -12,24 +12,24 @@ OS=""
 
 # --- Helper Functions ---
 print_info() {
-    echo -e "\033[34m[INFO]\033[0m "
+    echo -e "\033[34m[INFO]\033[0m $*"
 }
 
 print_success() {
-    echo -e "\033[32m[SUCCESS]\033[0m "
+    echo -e "\033[32m[SUCCESS]\033[0m $*"
 }
 
 print_warning() {
-    echo -e "\033[33m[WARNING]\033[0m "
+    echo -e "\033[33m[WARNING]\033[0m $*"
 }
 
 print_error() {
-    echo -e "\033[31m[ERROR]\033[0m "
+    echo -e "\033[31m[ERROR]\033[0m $*" >&2
     exit 1
 }
 
 command_exists() {
-    command -v "" &>/dev/null
+    command -v "$1" &>/dev/null
 }
 
 detect_os() {
