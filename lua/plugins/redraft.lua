a1336ac5169bab99d98932a6a5e64e8b0ff39157
Ole Henning Vårdal <olevaar@gmail.com>
2025-10-23 18:03:31 +0200

    Add nvim-redraft

---
+++ lua/plugins/redraft.lua
@@ -0,0 +1,8 @@
+return {
+  'jim-at-jibba/nvim-redraft',
+  event = 'VeryLazy',
+  build = 'cd ts && npm install && npm run build',
+  opts = {
+    -- See Configuration section for options
+  },
+}
