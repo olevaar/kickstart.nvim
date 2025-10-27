96f24818c051f76bc4a47595813ee6e1854a4d41
Ole Henning Vårdal <olevaar@gmail.com>
2025-10-27 20:43:57 +0100

    Change markdown to render-markdown.

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,9 +1,9 @@
 return {
-  'MeanderingProgrammer/markdown.nvim', -- formerly render-markdown.nvim
+  'MeanderingProgrammer/render-markdown.nvim',
   ft = { 'markdown' },
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons',
   },
-  opts = {}, -- defaults are good
+  opts = {},
 }
