f0e779ff6d5102db36f2178428c9ff38bb12fd87
Ole Henning Vårdal <olevaar@gmail.com>
2025-10-29 12:32:32 +0100

    Fix render-markdown output in magenta.nvim
    Initial output is still not rendered properly. Requires interacting with
    or toggling the buffer (<leader>mt). But after that it works fine.

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -7,9 +7,21 @@ return {
   },
   opts = {
     file_types = { 'markdown' },
-    -- Allow rendering in nofile buffers (needed for Magenta output)
     exclude = {
       buftypes = {},
     },
   },
+  config = function(_, opts)
+    require('render-markdown').setup(opts)
+
+    vim.api.nvim_create_autocmd('BufEnter', {
+      pattern = '*',
+      callback = function(args)
+        local bufname = vim.api.nvim_buf_get_name(args.buf)
+        if bufname:match '%[Magenta Chat%]' or bufname:match '%[Magenta Input%]' then
+          vim.bo[args.buf].filetype = 'markdown'
+        end
+      end,
+    })
+  end,
 }
