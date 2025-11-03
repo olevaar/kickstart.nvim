de8a94456792e07def1406845ad8bac333ca61e5
Ole Henning Vårdal <olevaar@gmail.com>
2025-11-03 17:26:56 +0100

    Change font

--- init.lua
+++ init.lua
@@ -20,7 +20,7 @@ vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.confirm = true
 
-vim.o.guifont = 'RobotoMono Nerd Font:h12'
+vim.o.guifont = 'UbuntuMono Nerd Font:h12'
 
 vim.diagnostic.config {
   virtual_text = {
