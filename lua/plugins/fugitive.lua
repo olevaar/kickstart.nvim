8f045d8846b2dc6741ff4919d13558df4a04e8f3
Ole Henning Vårdal <olevaar@gmail.com>
2025-11-04 19:23:39 +0100

    Add Gvdiffsplit! keybind

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,6 +4,7 @@ return {
   keys = {
     { '<leader>gs', '<cmd>Git<cr>', desc = 'Git status' },
     { '<leader>gd', '<cmd>vert Gvdiffsplit<cr>', desc = 'Git diff' },
+    { '<leader>gv', '<cmd>vert Gvdiffsplit!<cr>', desc = 'Git diff (three-way)' },
     { '<leader>gb', '<cmd>Git blame<cr>', desc = 'Git blame' },
   },
 }
