0351a620c86098160a922df635201e2fb909e448
Ole Henning Vårdal <olevaar@gmail.com>
2025-12-03 22:26:37 +0100

    Change java launch

--- lua/configs/jdtls.lua
+++ lua/configs/jdtls.lua
@@ -12,22 +12,46 @@ function M.config()
   local mason_path = vim.fn.stdpath 'data' .. '/mason'
   local jdtls_root = mason_path .. '/packages/jdtls'
 
+  local lombok_jar = mason_path .. 'lombok.jar'
+
+  local launcher_jar = (function()
+    local matches = vim.fn.glob(mason_path .. '/plugins/org.eclipse.equinox.launcher_*.jar', true, true)
+    if type(matches) == 'table' and #matches > 0 then
+      return matches[1]
+    else
+      return mason_path .. '/plugins/org.eclipse.equinox.launcher.jar'
+    end
+  end)()
+
   local os_config = (function()
     if vim.fn.has 'mac' == 1 then
-      return 'config_mac'
+      return '/config_mac'
     elseif vim.fn.has 'win32' == 1 then
-      return 'config_win'
+      return '/config_win'
     else
-      return 'config_linux'
+      return '/config_linux'
     end
   end)()
 
   local cmd = {
-    jdtls_root .. '/bin/jdtls',
+    'java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    'Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '-javaagent:' .. lombok_jar,
+    '-jar',
+    launcher_jar,
+    '-configuration',
+    jdtls_root .. os_config,
     '-data',
     workspace_dir,
-    '--jvm-args',
-    '-Xms512m',
   }
 
   local root_markers = { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }
