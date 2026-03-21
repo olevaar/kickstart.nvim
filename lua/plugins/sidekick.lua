68527a63d7b944823752036ad0359b14ac1a0007
Ole Henning Vårdal <olevaar@gmail.com>
2026-03-21 16:31:36 +0100

    Remove example keybinding

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -66,13 +66,5 @@ return {
       mode = { 'n', 'x' },
       desc = 'Sidekick Select Prompt',
     },
-    -- Example of a keybinding to open Claude directly
-    {
-      '<leader>ac',
-      function()
-        require('sidekick.cli').toggle { name = 'claude', focus = true }
-      end,
-      desc = 'Sidekick Toggle Claude',
-    },
   },
 }
